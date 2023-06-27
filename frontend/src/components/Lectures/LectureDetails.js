7e2b4c74a0948b815e0be867880d409f6c7287a3
alimuratkuslu <alimuratkuslu@gmail.com>
2023-06-27 14:52:40 +0300

    Implemented letter grades and frontend developmentü

--- frontend/src/components/Lectures/LectureDetails.js
+++ frontend/src/components/Lectures/LectureDetails.js
@@ -2,13 +2,26 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import Navbar from '../Navbar';
-import { Box, Grid, Typography, Card, CardContent } from '@material-ui/core';
+import { Box, Grid, Typography, Card, CardContent, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+    table: {
+      minWidth: 650,
+    },
+    tableHead: {
+      backgroundColor: theme.palette.primary.main,
+    },
+    tableHeadCell: {
+      color: theme.palette.common.white,
+    },
+  }));
 
 const LectureDetails = () => {
   const { lectureId } = useParams();
   const [lecture, setLecture] = useState(null);
   const [exams, setExams] = useState(null);
   const [students, setStudents] = useState({});
+  const classes = useStyles();
 
   useEffect(() => {
     fetchLectureDetails();
@@ -42,6 +55,34 @@ const LectureDetails = () => {
     }
   };
 
+  const getLetterGrade = (note) => {
+    if (note >= 97 && note <= 100) {
+      return 'A+';
+    } else if (note >= 93 && note <= 96) {
+      return 'A';
+    } else if (note >= 90 && note <= 92) {
+      return 'A-';
+    } else if (note >= 87 && note <= 89) {
+      return 'B+';
+    } else if (note >= 83 && note <= 86) {
+      return 'B';
+    } else if (note >= 80 && note <= 82) {
+      return 'B-';
+    } else if (note >= 77 && note <= 79) {
+      return 'C+';
+    } else if (note >= 73 && note <= 76) {
+      return 'C';
+    } else if (note >= 70 && note <= 72) {
+      return 'C-';
+    } else if (note >= 67 && note <= 69) {
+      return 'D+';
+    } else if (note >= 65 && note <= 66) {
+      return 'D';
+    } else {
+      return 'F';
+    }
+  };
+
   if (!lecture || !exams) {
     return <div>Loading lecture details...</div>;
   }
@@ -56,22 +97,35 @@ const LectureDetails = () => {
           <Grid item xs={12}>
             <Typography variant="h4">Lecture Details</Typography>
           </Grid>
-          <Grid item xs={12}>
+          <Grid item xs={12} style={{textAlign: 'center'}}>
             <Typography variant="body1">Lecture ID: {lecture._id}</Typography>
           </Grid>
-          <Grid item xs={12}>
-            <Typography variant="body1">Lecture Name: {lecture.name}</Typography>
+          <Grid item xs={12} style={{textAlign: 'center'}}>
+            <Typography variant="body1">Lecture Name: {lecture.name} {lecture.lectureNumber}</Typography>
           </Grid>
           <Grid item xs={12}>
             <Typography variant="body1">Enrolled Students:</Typography>
-            <ul>
-              {enrolledStudentIds.map((student) => (
-                <li key={student._id}>
-                  {students[student._id] && <span>{students[student._id]} - </span>}
-                  {student._id}
-                </li>
-              ))}
-            </ul>
+            <br />
+                <TableContainer component={Paper}>
+                    <Table className={classes.table}>
+                        <TableHead className={classes.tableHead}>
+                        <TableRow>
+                            <TableCell className={classes.tableHeadCell}>Full Name</TableCell>
+                            <TableCell className={classes.tableHeadCell}>Student Number</TableCell>
+                            <TableCell className={classes.tableHeadCell}>Student Email</TableCell>
+                        </TableRow>
+                        </TableHead>
+                        <TableBody>
+                        {enrolledStudentIds.map((student) => (
+                            <TableRow key={student._id}>
+                            <TableCell>{students[student._id]}</TableCell>
+                            <TableCell>{student.studentNumber}</TableCell>
+                            <TableCell>{student.email}</TableCell>
+                            </TableRow>
+                        ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
           </Grid>
           <Grid item xs={12}>
             <Typography variant="h5">Exams:</Typography>
@@ -81,15 +135,27 @@ const LectureDetails = () => {
                   <Typography variant="h6">Exam Number: {exam.examNumber}</Typography>
                   <Typography variant="body1">Date: {new Date(exam.date).toLocaleDateString('en-GB')}</Typography>
                   <Typography variant="body1">Type: {exam.type}</Typography>
-                  <Typography variant="body1">Notes:</Typography>
-                  <ul>
-                    {Object.entries(exam.notes).map(([studentId, note]) => (
-                      <li key={studentId}>
-                        {students[studentId] && <span>{students[studentId]} - </span>}
-                        {note}
-                      </li>
-                    ))}
-                  </ul>
+                  <br />
+                  <TableContainer component={Paper}>
+                    <Table className={classes.table}>
+                      <TableHead className={classes.tableHead}>
+                        <TableRow>
+                          <TableCell className={classes.tableHeadCell}>Name</TableCell>
+                          <TableCell className={classes.tableHeadCell}>Note</TableCell>
+                          <TableCell className={classes.tableHeadCell}>Grade</TableCell>
+                        </TableRow>
+                      </TableHead>
+                      <TableBody>
+                        {Object.entries(exam.notes).map(([studentId, note]) => (
+                          <TableRow key={studentId}>
+                            <TableCell>{students[studentId]}</TableCell>
+                            <TableCell>{note}</TableCell>
+                            <TableCell>{getLetterGrade(note)}</TableCell>
+                          </TableRow>
+                        ))}
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
                 </CardContent>
               </Card>
             ))}
