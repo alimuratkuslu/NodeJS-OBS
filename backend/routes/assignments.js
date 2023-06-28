0d792e608fff9b84d6d277dc8d9ab5bfa6364ed8
alimuratkuslu <alimuratkuslu@gmail.com>
2023-06-28 11:41:44 +0300

    Implemented CRUD endpoints for assignment

--- backend/routes/assignments.js
+++ backend/routes/assignments.js
@@ -35,10 +35,38 @@ router.route('/add').post((req, res) => {
         endDate,
         pdfFile,
         lectureName,
-        lectureName,});
+        notes,});
 
         newAssignment.save()
         .then(() => res.json('Assignment Added'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').get((req, res) => {
+    Assignment.findById(req.params.id)
+        .then(assignment => res.json(assignment))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+    Assignment.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Assignment Deleted'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').put((req, res) => {
+    Assignment.findById(req.params.id)
+        .then(assignment => {
+            assignment.name = req.body.name;
+            assignment.description = req.body.description;
+            assignment.startDate = req.body.startDate;
+            assignment.endDate = req.body.endDate;
+            assignment.lectureName = req.body.lectureName;
+
+            assignment.save()
+                .then(() => res.json('Assignment Updated'))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
